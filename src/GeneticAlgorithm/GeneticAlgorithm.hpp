ac73029c93d9daf6e80242bffae118db5ac9b5c5
Paulo Guilherme <pauloguilherme@usp.br>
2022-09-05 16:42:27 -0300

    Add class to represent the GA

---
+++ src/GeneticAlgorithm/GeneticAlgorithm.hpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include "PopElement.cpp"
+
+#ifndef GENETIC_ALGORITHM
+#define GENETIC_ALGORITHM
+
+class GeneticAlgorithm{
+    public:
+        GeneticAlgorithm(int popSize, int numGenerations, int numGenes,
+                        long double (*fitness)(std::vector<long double>),
+                        long double minValue, long double maxValue,
+                        long double mutationRate, long double mutationParam,
+                        int kTournament, std::string Crossover);
+        void showBestSolution();
+        void run();
+
+    private:
+        PopElement bestAns;
+        std::vector<PopElement> pop, parents;
+        long double minValue, maxValue, mutationRate, mutationParam;
+        int popSize, numGenerations, numGenes, kTournament;
+        long double (*fitness)(std::vector<long double>);
+        std::vector<std::vector<long double>> (GeneticAlgorithm::*crossover)(int, int);
+
+        void startPop();
+        void evalPop();
+        PopElement tournamentSelection();
+        void parentsSelection();
+        std::vector<std::vector<long double>> onePointCrossover(int p1, int p2);
+        std::vector<std::vector<long double>> twoPointsCrossover(int p1, int p2);
+        void createNewGeneration();
+        void uniformMutation();
+        void showBestAns();
+
+        long double uniformRandomNum(long double MinValue, long double MaxValue);
+        std::vector<long double> uniformVectorRandomNum(int size);
+}; 
+#endif
